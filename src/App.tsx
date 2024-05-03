3055a153ffba7dc5171ca1e1594e66e046feeb6e
Nycollas Kaique <nycollaskaique@hotmail.com>
2024-05-03 14:48:28 -0300

    feat: adicionado caixa de confirmação para deletar cliente. refactor: mudou exibição de clientes de lista ul para table. style: adicionado estilos css com tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState, useRef, useEffect } from "react";
 import buscacep from "./services/buscacep";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function App() {
   interface Cliente {
@@ -20,6 +21,7 @@ function App() {
   const [clientes, setClientes] = useState<Cliente[]>(getLocalStorage);
   const [index, setIndex] = useState<number>(0);
   const [atualizando, setAtualizando] = useState(false);
+  const [toggle, setToggle] = useState(false);
   const nome = useRef<HTMLInputElement>(null);
   const email = useRef<HTMLInputElement>(null);
   const cep = useRef<HTMLInputElement>(null);
@@ -76,50 +78,120 @@ function App() {
     nome.current.value = "";
     email.current.value = "";
     cep.current.value = "";
+    setIndex(0);
+    setAtualizando(false);
   }
 
   function editar(index) {
+    setToggle(true);
     const lista = [...clientes];
     nome.current.value = lista[index].nome;
     email.current.value = lista[index].email;
     cep.current.value = lista[index].cep;
+    nome.current.focus();
     setIndex(index);
     setAtualizando(true);
   }
 
   function excluir(index: number) {
     const lista = [...clientes];
-    lista.splice(index, 1);
-    setClientes(lista);
+    if (confirm(`Deseja excluir ${lista[index].nome}?`)) {
+      lista.splice(index, 1);
+      setClientes(lista);
+      limpar();
+    }
+  }
+  function handleToggle() {
+    setToggle(!toggle);
   }
   useEffect(() => {
     localStorage.setItem("clientes", JSON.stringify(clientes));
   }, [clientes]);
   return (
-    <>
-      <h1>Cadastro de Clientes</h1>
-      <form onSubmit={(e) => handleSubmit(e)}>
-        <label>Nome</label>
-        <input type="text" ref={nome} />
-        <label>E-mail</label>
-        <input type="email" ref={email} />
-        <label>CEP</label>
-        <input type="text" ref={cep} />
-        <button type="submit">{atualizando ? "Atualizar" : "Cadastrar"}</button>
-        <button onClick={() => limpar()}>Limpar</button>
+    <div className="w-full min-h-screen bg-stone-400 background py-4 text-gray-700">
+      <div className="flex justify-center items-center gap-4 font-bold text-lg mb-4 flex-col gap-4 sm:flex-row">
+        <h1 className="text-center w-fit bg-white shadow-md rounded px-8 py-2">
+          Cadastro de Clientes
+        </h1>
+        <button
+          className=" w-10 h-10 shrink-0 btn-color rounded-[50%]"
+          onClick={() => handleToggle()}
+        >
+          <i className="fa-solid fa-plus "></i>
+        </button>
+      </div>
+
+      <form
+        onSubmit={(e) => handleSubmit(e)}
+        // w-full flex justify-center gap-2 items-center
+        className={toggle ? "show" : "hide"}
+      >
+        <div className="mb-4">
+          <label className="block text-sm font-bold mb-2">Nome</label>
+          <input
+            type="text"
+            ref={nome}
+            className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-sm font-bold mb-2">E-mail</label>
+          <input
+            type="email"
+            ref={email}
+            className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+        <div className="mb-6">
+          <label className="block text-sm font-bold mb-2">CEP</label>
+          <input
+            type="text"
+            ref={cep}
+            className="shadow appearance-none border rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline"
+          />
+        </div>
+        <div className="flex flex-col gap-4 sm:flex-row  items-center justify-between">
+          <button type="submit" className="btn btn-color">
+            {atualizando ? "Atualizar" : "Cadastrar"}
+          </button>
+          <button onClick={() => limpar()} className="btn btn-color">
+            {atualizando ? "Cancelar" : "Limpar"}
+          </button>
+        </div>
       </form>
-      <ul>
-        {clientes.map((c, index) => (
-          <li key={index}>
-            <span>Nome {c?.nome}</span>
-            <span>E-mail {c?.email}</span>
-            <span>Endereço {c?.endereco}</span>
-            <button onClick={() => editar(index)}>Editar</button>
-            <button onClick={() => excluir(index)}>Excluir</button>
-          </li>
-        ))}
-      </ul>
-    </>
+      <div className="max-w-[90%] w-[90%] mx-auto overflow-x-auto rounded">
+        <table className=" bg-white shadow-md rounded mx-auto ">
+          <thead>
+            <tr className="border-b-2 border-lime-400 ">
+              <th className="p-2 font-bold">Nome</th>
+              <th className="p-2  font-bold">E-mail</th>
+              <th className="p-2  font-bold">Endereço</th>
+              <th className="p-2  font-bold">Editar</th>
+              <th className="p-2  font-bold">Excluir</th>
+            </tr>
+          </thead>
+          <tbody>
+            {clientes.map((c, index) => (
+              <tr key={index} className="border-b-2 h-[3em]">
+                <td className="px-2">{c?.nome}</td>
+                <td className="px-2">{c?.email}</td>
+                <td className="px-2">{c?.endereco}</td>
+                <td className="text-center">
+                  <button onClick={() => editar(index)}>
+                    <i className="fa-regular fa-pen-to-square text-lg hover:text-lime-400 transition-all"></i>
+                  </button>
+                </td>
+                <td className="text-center">
+                  <button onClick={() => excluir(index)}>
+                    <i className="fa-regular fa-trash-can text-lg hover:text-lime-400 transition-all"></i>
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
   );
 }
 
